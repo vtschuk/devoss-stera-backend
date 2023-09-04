f56230bb3454b662b41ef5a69ad2121cfc67e66a
xgarnet <vt7970170@gmail.com>
2023-09-04 10:55:03 +0200

    graphql example

---
+++ src/main/kotlin/com/example/strera/model/Author.kt
@@ -0,0 +1,21 @@
+package com.example.strera.model
+
+import java.util.*
+
+@JvmRecord
+data class Author(val id: String, val firstName: String, val lastName: String) {
+    companion object {
+        private val authors = Arrays.asList(
+            Author("author-1", "Joshua", "Bloch"),
+            Author("author-2", "Douglas", "Adams"),
+            Author("author-3", "Bill", "Bryson")
+        )
+
+        fun getById(id: String): Author? {
+            return authors.stream()
+                .filter { author: Author? -> author!!.id == id }
+                .findFirst()
+                .orElse(null)
+        }
+    }
+}
---
+++ src/main/kotlin/com/example/strera/model/Book.kt
@@ -0,0 +1,21 @@
+package com.example.strera.model
+
+import java.util.*
+
+@JvmRecord
+data class Book(val id: String, val name: String, val pageCount: Int, val authorId: String) {
+    companion object {
+        private val books = Arrays.asList(
+            Book("book-1", "Effective Java", 416, "author-1"),
+            Book("book-2", "Hitchhiker's Guide to the Galaxy", 208, "author-2"),
+            Book("book-3", "Down Under", 436, "author-3")
+        )
+
+        fun getById(id: String): Book? {
+            return books.stream()
+                .filter { book: Book? -> book!!.id == id }
+                .findFirst()
+                .orElse(null)
+        }
+    }
+}
