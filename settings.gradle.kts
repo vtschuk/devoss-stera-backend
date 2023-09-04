9bc8a37dec1e4679fd719f6b83ee9b61e91c45b0
xgarnet <vt7970170@gmail.com>
2023-09-04 08:22:18 +0200

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "devoss-stera-backend"
---
+++ src/main/kotlin/com/example/devosssterabackend/DevossSteraBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.devosssterabackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevossSteraBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<DevossSteraBackendApplication>(*args)
+}
